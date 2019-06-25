164bb349e602d44225b0dcc9761c31a8f932c294
15173760803 <2435118603@qq.com>
2019-06-25 12:23:28 +0800

    react路由

---
+++ src/components/routerfu1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class routerfu1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>一级路由导航</h1>
+            </div>
+        )
+    }
+}
